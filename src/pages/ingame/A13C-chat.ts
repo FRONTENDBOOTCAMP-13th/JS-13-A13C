4f24a9bc050b4aa35c5fe2d4a694e7756cd5ca9f
jungwoo0601 <pjw1346799@naver.com>
2025-05-15 17:56:48 +0900

    [feat] 로비(방만들기, 참여하기)

--- src/pages/ingame/A13C-chat.ts
+++ src/pages/ingame/A13C-chat.ts
@@ -1,4 +1,4 @@
-import io  from "socket.io-client";
+import io from "socket.io-client";
 
 /**
  * 채팅 메시지의 구조를 정의하는 인터페이스
@@ -87,7 +87,7 @@ export interface RoomsResponse {
 }
 
 // npm i @types/socket.io-client 필요
-export const socket = io("ws://fesp-api.koyeb.app/febc13-chat/team01");
+export const socket = io("ws://fesp-api.koyeb.app/febc13-chat/team02");
 
 /**
  * 소켓 연결 이벤트 리스너
