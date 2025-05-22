24896c7d18be4f62a72a86aacdcc973bdb7cbbe7
ychanwoo <chxan02@naver.com>
2025-05-22 14:34:44 +0900

    [fix] index.ts 오류 수정 완료

--- src/script/ingame/index.ts
+++ src/script/ingame/index.ts
@@ -30,29 +30,24 @@ async function getRoomInfo() {
       nickName,
     };
 
-if(roomId && nickName) {
-  const params: JoinRoomParams = {
-    roomId,
-    user_id: nickName,
-    nickName,
-  };
-
-  const joinRoomRes = await joinRoom(params);
-  console.log('joinRoomRes', joinRoomRes);
-  if(joinRoomRes.ok){
-    setUserId(nickName);
-    const playerList = Object.values(joinRoomRes.roomInfo.memberList);
-    console.log('플레이어 목록', playerList);
-    setPlayerList(playerList);
-    setRoomName(joinRoomRes.roomInfo.roomName);
-    refreMembers(playerList);
-    document.querySelector('#connectedRoom')!.innerHTML = `방이름: ${getRoomName()} (${getPlayerCount()}/5)`;
-    alert(`"${getRoomName()}" 방에 입장하였습니다.`);
-  }else{
-    alert(joinRoomRes.message);
-}else {
+    const joinRoomRes = await joinRoom(params);
+    console.log('joinRoomRes', joinRoomRes);
+    if(joinRoomRes.ok){
+      setUserId(nickName);
+      const playerList = Object.values(joinRoomRes.roomInfo.memberList);
+      console.log('플레이어 목록', playerList);
+      setPlayerList(playerList);
+      setRoomName(joinRoomRes.roomInfo.roomName);
+      refreMembers(playerList);
+      document.querySelector('#connectedRoom')!.innerHTML = `방이름: ${getRoomName()} (${getPlayerCount()}/5)`;
+      alert(`"${getRoomName()}" 방에 입장하였습니다.`);
+    }else{
+      alert(joinRoomRes.message);
+    }
+  } else {
     alert("잘못된 경로로 접근했습니다. 로비로 이동합니다.");
     location.href = "/src/pages/lobby.html";
+  }
 }
 
 getRoomInfo();
@@ -65,17 +60,19 @@ getRoomInfo();
 socket.on("members", refreMembers);
 
 function refreMembers(members: Player[]) {
+
   setPlayerList(Object.values(members));
+
   const playerList = getPlayerList();
   let position = 1;
+
   for (let i = 0; i < playerList.length; i++) {
     const player = playerList[i];
     console.log(getUserId(), player.nickName);
+
     if (getUserId() !== player.nickName) {
-      document.querySelector(`#nickname-${position++}`)!.textContent =
-        player.nickName;
+      document.querySelector(`#nickname-${position++}`)!.textContent = player.nickName;
     }
   }
-  document.querySelector("#connectedRoom")!.innerHTML =
-    `방이름: ${getRoomName()} (${getPlayerCount()}/5)`;
-}
+  document.querySelector("#connectedRoom")!.innerHTML = `방이름: ${getRoomName()} (${getPlayerCount()}/5)`;
+};
